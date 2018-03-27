2f6e88a170bcca715a8af662026b0037d43402a4
Yvonne <hemiao3@100tal.com>
2018-03-27 21:11:47 +0800

    TemplateSyntax

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Demo Links</h2>
+    <ul>
+      <li v-for="item in linkList" :key="item.link">
+        <router-link :to="item.link">
+          item.title
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Vue.js Demo',
+      linkList: [
+        { title: 'templateSyntax', link: '/templateSyntax' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/TemplateSyntax.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input name="test" v-model="msg">
+    <p>{{msg}}</p>
+    <p v-once>{{msg}}</p>
+    <p v-html="html">test</p>
+    <p>test <span v-html="html">no</span></p>
+    <button disabled=undefined>test v-bind1</button>
+    <button disabled=null>test v-bind2</button>
+    <button disabled=false>test v-bind3</button>
+    <button disabled=true>test v-bind4</button>
+    <button :disabled="disabled1">test v-bind5</button>
+    <button :disabled="disabled2">test v-bind5</button>
+    <button :disabled="disabled3">test v-bind5</button>
+    <button :disabled="disabled4">test v-bind5</button>
+    <p>{{globalVariable1?globalVariable1:'访问不到自定义的全局变量'}}</p>
+    <p>{{Math.max(1,2,3)+'-可以访问原有的全局变量'}}</p>
+    <a :href="url">test :</a>
+    <a :href="url" @click.prevent="clickButton" >test prevent</a>
+  </div>
+</template>
+
+<script>
+window.globalVariable1 = 'xiaohei'
+export default{
+  data () {
+    return {
+      msg: 'xiaohei',
+      html: '<span style="color:red">red</span>',
+      disabled1: null,
+      disabled2: undefined,
+      disabled3: false,
+      disabled4: true,
+      url: 'http://www.yvonne.party'
+    }
+  },
+  methods: {
+    clickButton: function () {
+      console.log('click!')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input{
+    border: 1px solid #333;
+  }
+</style>
